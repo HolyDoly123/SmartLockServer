736a88fb84b0a6f575484694be0d07670f2820d6
HolyDoly123 <vladimirudakovhd@gmail.com>
2023-02-26 11:55:25 +0300

    Added prod properties

--- src/main/java/ru/rudakov/smartlockserver/SmartLockServerApplication.java
+++ src/main/java/ru/rudakov/smartlockserver/SmartLockServerApplication.java
@@ -1,9 +1,11 @@
 package ru.rudakov.smartlockserver;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class SmartLockServerApplication {
 
     public static void main(String[] args) {
